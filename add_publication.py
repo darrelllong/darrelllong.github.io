3abb7f774fa913ce1149f623dcc99b27f5188c84
Eugene Chou <euchou@ucsc.edu>
2020-09-01 21:09:32 -0700

    looking into opening up the database

---
+++ add_publication.py
@@ -0,0 +1,85 @@
+import tkinter
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+import psycopg2
+import psycopg2.extras
+import sys
+
+labels = [
+    "pubid",
+    "title",
+    "booktitle",
+    "authors",
+    "date",
+    "file",
+    "abstract",
+    "bibtex",
+    "type"
+]
+
+fields = {}
+entries = {}
+
+def create_form(window):
+    scrollbar = Scrollbar(window)
+
+    for r, label in enumerate(labels):
+        fields[label] = Label(window, text=label)
+        fields[label].pack(padx=10, pady=1)
+
+        if label == "abstract" or label == "bibtex":
+            entries[label] = ScrolledText(window, height=8, relief="solid")
+        else:
+            entries[label] = Entry(window)
+
+        entries[label].pack(padx=10, pady=3)
+
+    submit = Button(window, text="Add", fg="Black", bg="Red", command=add_pub)
+    submit.pack(pady=6)
+
+def add_pub():
+    pubid = f"\'{entries['pubid'].get()}\'"
+    title = f"\'{entries['title'].get()}\'"
+    booktitle = f"\'{entries['booktitle'].get()}\'"
+    authors = f"\'{entries['authors'].get()}\'"
+    date = f"\'{entries['date'].get()}\'"
+    file = f"\'{entries['file'].get()}\'"
+    abstract = f"\'{entries['abstract'].get('1.0', tkinter.END)[:-1]}\'"
+    bibtex = f"\'{entries['bibtex'].get('1.0', tkinter.END)[:-1]}\'"
+    type = f"\'{entries['type'].get()}\'"
+
+    query = (
+        "INSERT INTO "
+        "publications(pubid, title, booktitle, authors, date, file, abstract, bibtex, type) "
+        f"VALUES ({pubid}, {title}, {booktitle}, {authors}, {date}, {file}, {abstract}, {bibtex}, {type});"
+    )
+
+    try:
+        conn = psycopg2.connect(
+            user = "Eugene",
+            host = "127.0.0.1",
+            database = "website"
+        )
+
+        with conn:
+            cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            cursor.execute(query)
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error connecting PostgreSQL:", error)
+
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
+
+    sys.exit(0)
+
+window = Tk()
+window.title("Publications Adder")
+window.geometry("640x800")
+window.minsize("640", "800")
+
+create_form(window)
+
+window.mainloop()
---
+++ update_publications.py
@@ -0,0 +1,76 @@
+import psycopg2
+import psycopg2.extras
+import re
+
+#
+# Creates markdown file for a row in the database.
+#
+def create_md(row):
+    with open(f"_publications/{row['date']}-{row['pubid']}.md", "w") as file:
+        # Frontmatter prelude.
+        file.write("---\n")
+
+        # Layout.
+        file.write("layout: publication\n")
+
+        # Title.
+        file.write(f"title: Darrell Long | {row['title']}\n")
+
+        # Header (same as title but without the name).
+        file.write(f"header: {row['title']}\n")
+
+        # Permalink.
+        file.write(f"permalink: /publications/{row['pubid']}/\n")
+
+        # Authors.
+        file.write(f"authors: {row['authors']}\n")
+
+        # Date.
+        file.write(f"date: {row['date']}\n")
+
+        # File source.
+        file.write(f"file: {row['file']}\n")
+
+        # Excerpt (first two sentences of abstract).
+        excerpt = ' '.join(re.split(r'(?<=[.:;])\s', row['abstract'])[:2])
+        file.write(f"excerpt: >\n  {excerpt}\n")
+
+        # Frontmatter postlude.
+        file.write("---\n")
+
+        # Book title.
+        file.write(f"\nAppeared in _{row['booktitle']}_.\n")
+
+        # Abstract.
+        file.write("\n# Abstract\n\n")
+        file.write(f"{row['abstract']}\n")
+
+        # BibTex.
+        file.write("\n# BibTeX\n\n")
+        file.write(f"```latex\n{row['bibtex']}\n```\n")
+
+def main():
+    try:
+        conn = psycopg2.connect(
+            user = "Eugene",
+            host = "127.0.0.1",
+            database = "website"
+        )
+
+        with conn:
+            cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            cursor.execute("SELECT * FROM public.publications;")
+
+            for row in cursor:
+                create_md(row)
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error connecting PostgreSQL:", error)
+
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
+
+if __name__ == "__main__":
+    main()
