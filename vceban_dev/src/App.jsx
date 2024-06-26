e013a61b217d67f60543db983cc186621c2ae982
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2024-06-26 14:43:06 -0700

    upd preview path

--- vceban_dev/src/App.jsx
+++ vceban_dev/src/App.jsx
@@ -10,7 +10,7 @@ import Footer from "./components/Footer";
 export default function App() {
   return (
     <>
-      <Router>
+      <Router basename={"https://darrelllong.github.io/react/"}>
         <ContextProvider>
           <Header />
           <Main />
