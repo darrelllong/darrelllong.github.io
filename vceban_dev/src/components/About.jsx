bb2f260b07ccb4c53636154a6620fb57f5ae0376
Vladimir Ceban <v3ceban@proton.me>
2024-06-27 22:14:42 -0700

    font optimization and mobile image fix

--- vceban_dev/src/components/About.jsx
+++ vceban_dev/src/components/About.jsx
@@ -2,8 +2,6 @@
 import React from "react";
 // Components
 import Accordion from "./Accordion";
-// Assets
-import picture from "../assets/img/DLong.avif";
 // Styles
 import "../assets/css/about.scss";
 
@@ -40,7 +38,7 @@ export default function About() {
     <>
       <section className="aboutSection dottedBorder">
         <h2>About</h2>
-        <img src={picture} alt="Darrell Long" />
+        <img src="/react/DLong.avif" alt="Darrell Long" />
         <p>
           Dr. Darrell D. E. Long is a Distinguished Visiting Scholar at Santa
           Clara University and is Distinguished Professor of Engineering,
